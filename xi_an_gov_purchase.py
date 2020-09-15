e4963bacd0c119dd8bca4a85e2319226419a48fa
陈献辉xianhuichen <xianhuichen@clubfactory.com>
2020-09-15 19:59:27 +0800

    1

--- xi_an_gov_purchase.py
+++ xi_an_gov_purchase.py
@@ -1,9 +1,11 @@
 import requests
 from lxml import etree
 
-url="http://www.ccgp-shaanxi.gov.cn/notice/noticeaframe.do?noticetype=3&isgovertment="
-params ={'parameters[''title'']': '厨房',
-         'parameters[''regionguid'']': '6101'}
-r = requests.post(url,params=params)
+url = "http://www.ccgp-shaanxi.gov.cn/notice/noticeaframe.do?noticetype=3&isgovertment="
+params = {'parameters[''title'']': '厨房',
+          'parameters[''regionguid'']': '6101'}
+r = requests.post(url, params=params)
 content = r.content
-print(str(content,'utf-8'))
+selector = etree.HTML(content)
+title=selector.xpath('//title/text()')
+print(str(title[0], 'utf-8'))
